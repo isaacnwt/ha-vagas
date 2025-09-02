9513f021daee03134ef2bb10a6b9db122cf94677
isaacnwt <isaacnewton02@hotmail.com>
2025-09-02 20:50:04 -0300

    feat: funcionamento do toggle de celular

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/havagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/havagas/MainActivity.kt
@@ -1,20 +1,37 @@
 package br.edu.ifsp.scl.ads.prdm.sc3014789.havagas
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3014789.havagas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        with(activityMainBinding) {
+            setContentView(activityMainBinding.root)
+            setupListeners()
+        }
+    }
+
+    private fun setupListeners() = with(activityMainBinding) {
+        setupCelularToggle()
+    }
+
+    private fun setupCelularToggle() = with(activityMainBinding) {
+        adicionarCelularCb.setOnClickListener { view ->
+            if ((view as CheckBox).isChecked) {
+                celularEt.visibility = View.VISIBLE
+            } else {
+                celularEt.visibility = View.GONE
+                celularEt.setText("")
+            }
         }
     }
 }
