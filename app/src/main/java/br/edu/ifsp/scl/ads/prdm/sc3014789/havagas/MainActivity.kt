5bef21f7a25b46d9fcdeffbcbe947c4cc5152797
isaacnwt <isaacnewton02@hotmail.com>
2025-09-03 00:04:32 -0300

    feat: funcionamento do calendario para datra de nascimento

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/havagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/havagas/MainActivity.kt
@@ -1,10 +1,12 @@
 package br.edu.ifsp.scl.ads.prdm.sc3014789.havagas
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.ads.prdm.sc3014789.havagas.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     private val activityMainBinding: ActivityMainBinding by lazy {
@@ -22,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupListeners() = with(activityMainBinding) {
         setupCelularToggle()
+        setupDataNascimentoPicker()
     }
 
     private fun setupCelularToggle() = with(activityMainBinding) {
@@ -34,4 +37,26 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun setupDataNascimentoPicker() {
+        activityMainBinding.dataNascimentoEt.setOnClickListener {
+            val calendario = Calendar.getInstance()
+            val ano = calendario.get(Calendar.YEAR)
+            val mes = calendario.get(Calendar.MONTH)
+            val dia = calendario.get(Calendar.DAY_OF_MONTH)
+
+            val datePicker = DatePickerDialog(
+                this,
+                { _, anoSelecionado, mesSelecionado, diaSelecionado ->
+                    val dataFormatada =
+                        "%02d/%02d/%d".format(diaSelecionado, mesSelecionado + 1, anoSelecionado)
+                    activityMainBinding.dataNascimentoEt.setText(dataFormatada)
+                },
+                ano, mes, dia
+            )
+
+            datePicker.show()
+        }
+    }
+
 }
